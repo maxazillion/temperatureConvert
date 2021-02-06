643442f0f88c3b71333f6a80e0add81f003786cf
maxazillion <47362990+maxazillion@users.noreply.github.com>
2021-02-06 16:20:24 -0500

    add event listener to the first and second input to dynamically change opposite input

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+inputs[0].addEventListener("blur", () => {
+  // multiply by 1.8 and add 32
+  inputs[1].value = 0;
+  inputs[1].value = inputs[0].value * 1.8 + 32;
+});
+
+inputs[1].addEventListener("blur", () => {
+  // subtract 32 and multiply by . 5556 (or 5/9).
+  inputs[0].value = 0;
+  inputs[0].value = (inputs[1].value - 32) * (5 / 9);
+});
