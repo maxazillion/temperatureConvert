e21d02fe89ebfa92732d8d7f7878d61a5bdb7216
maxazillion <47362990+maxazillion@users.noreply.github.com>
2021-02-06 16:54:50 -0500

    change event listener to key up in order to catch the last value inputted

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 const inputs = document.querySelectorAll("input");
 console.log(inputs);
 
-inputs[0].addEventListener("blur", () => {
+inputs[0].addEventListener("keyup", () => {
   // multiply by 1.8 and add 32
   inputs[1].value = 0;
   inputs[1].value = inputs[0].value * 1.8 + 32;
 });
 
-inputs[1].addEventListener("blur", () => {
+inputs[1].addEventListener("keyup", () => {
   // subtract 32 and multiply by . 5556 (or 5/9).
   inputs[0].value = 0;
   inputs[0].value = (inputs[1].value - 32) * (5 / 9);
